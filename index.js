f91eea7a6102f9c1576d557caaae7d88f919aa3d
Muhammad Shoaib <shoaibmehar037@gmail.com>
2023-05-04 22:15:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,31 +30,32 @@ faqs.forEach((faq, index) => {
 
 // js for reviews 
 
-const reviews = document.querySelectorAll(".review_text");
+const reviews = document.querySelectorAll('.review_text');
 reviews.forEach((review) => {
   // Trim review text & Add read more button
-  const arr = review.innerHTML.split(" ");
+  const arr = review.innerHTML.split(' ');
   if (arr.length > 70) {
     // Cut to 70 words
     const newText = arr
       .filter((text, index) => {
         return index < 70;
       })
-      .join(" ");
+      .join(' ');
     review.innerHTML = newText;
     // Read more button
-    const readMoreButton = document.createElement("span");
-    readMoreButton.innerHTML = "Read more...";
-    readMoreButton.classList.add("read_more");
+    const readMoreButton = document.createElement('span');
+    readMoreButton.innerHTML = 'Read more...';
+    readMoreButton.classList.add('read_more');
     review.appendChild(readMoreButton);
     // Add onClick event to read more button
     if (readMoreButton) {
-      readMoreButton.addEventListener("click", () => {
-        review.innerHTML = arr.join(" ");
+      readMoreButton.addEventListener('click', () => {
+        review.innerHTML = arr.join(' ');
       });
     }
   }
 });
+
 
 // js for Instagram Widget
 
