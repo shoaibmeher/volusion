11691f3954d44e91e78a0a9e77cd7205294d6fff
Muhammad Shoaib <shoaibmehar037@gmail.com>
2023-05-11 10:00:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -62,3 +62,12 @@ reviews.forEach((review) => {
 (function(d, s, id) { var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript"));
 
 
+// js for olarkchat
+
+
+(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
+/ custom configuration goes here (www.olark.com/documentation) /
+olark.identify('8976-817-10-6014');
+
+
+
